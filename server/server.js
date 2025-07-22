5edd1e9608607367f8c51fa5078ded6559288250
Krishiv274 <krishiv270406+github@gmail.com>
2025-07-22 10:01:15 +0530

    Updating CORS

--- server/server.js
+++ server/server.js
@@ -40,7 +40,11 @@ io.on("connection", (socket) => {
 app.use(express.json({ limit: "4mb" }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://huddle-app-five.vercel.app/",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "token"],
